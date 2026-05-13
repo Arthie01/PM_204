1f6d8eeaa0d35cd6db85af6b762b212e26883725
Arthie01 <123045452@upq.edu.mx>
2026-05-12 22:36:47 -0600

    First commit, Js cliente y js servidor

---
+++ Practica1/logica.js
@@ -0,0 +1,3 @@
+        let nombre = prompt("Cual es tu nombre?: ")
+        console.log("hola: " + nombre + "!")
+        document.write("<h1>hola: " + nombre + "!</h1>")
---
+++ Practica1/servidor.js
@@ -0,0 +1,24 @@
+console.log("Hola mundo JS desde el servidor")
+
+/*Promedio 2 variables*/
+let edad1 = 11
+let edad2 = 33
+console.log("Edad promedio: ")
+console.log((edad1 + edad2)/2)
+
+/*Medir tiempo de procesos*/
+console.time("miProceso")
+
+    for(let i = 0; i < 10; i++){
+
+    }
+
+console.timeEnd("miProceso")
+
+/* Objetos tipo tabla*/
+let usuarios = [
+    {nombre: "Artemio", edad: "21"},
+    {nombre: "Keren", edad: "17"}
+]
+
+console.table(usuarios)
