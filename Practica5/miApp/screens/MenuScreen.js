9f0a8e3c041cfe88435f24447eb796d40f3dab10
Arthie01 <123045452@upq.edu.mx>
2026-06-16 17:02:36 -0600

    botones finales

--- Practica5/miApp/screens/MenuScreen.js
+++ Practica5/miApp/screens/MenuScreen.js
@@ -1,11 +1,15 @@
 /*ZONA1: importaciones compponentes y archivos*/
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
 import React, {useState} from 'react';
 import TarjetaScreen from './TarjetaScreen';
 import SafeAreaScreen from './SafeAreaScreen';
-
+import PressableScreen from './PressableScreen';
+import TextInputAlertScreen from './TextInputAlertScreen';
+import FlatListSectionScreen from './FlatListSectionScreen';
+import ImageBackgroundSlapshScreen from './ImageBackgroundSlapshScreen';
+import ModalBottomScreen from './ModalBottomScreen';
 
 export default function MenuScreen() {
     const [screen, setScreen] = useState('menu');
@@ -15,18 +19,60 @@ export default function MenuScreen() {
             return <TarjetaScreen/>
         case 'SafeArea':
             return <SafeAreaScreen/>
+        case 'Pressable':
+            return <PressableScreen/>
+        case 'TextInput':
+            return <TextInputAlertScreen/>
+        case 'FlatList':
+            return <FlatListSectionScreen/>
+        case 'Image':
+            return <ImageBackgroundSlapshScreen/>
+        case 'Activity':
+            return <ActivityIndicatorScreen/>
+        case 'Modal':
+            return <ModalBottomScreen/>
         case 'menu':
             default:
 
                 return (
-                    <View style={styles.container}>
-                        <Button title= 'Practica Tarjetas' onPress={() => setScreen('tarjetas')}/>
-                        <Button title = 'Practica SafeArea' onPress={() => setScreen('SafeArea')}/>
+                    <>
+                        <View style={styles.container}>
+                            <Button title= 'Practica Tarjetas' onPress={() => setScreen('tarjetas')}/>
+                        </View>
+
+                        <View style={styles.container}>
+                            <Button title= 'Practica SafeArea' onPress={() => setScreen('SafeArea')}/>
+                        </View>
+
+                        <View style={styles.container}>
+                            <Button title= 'Practica Pressable & swtich' onPress={() => setScreen('Pressable')}/>
+                        </View>
+
+                        <View style={styles.container}>
+                            <Button title= 'Practica TextInput & Alert' onPress={() => setScreen('TextInput')}/>
+                        </View>
+
+                        <View style={styles.container}>
+                            <Button title= 'Practica FlatList & section' onPress={() => setScreen('FlatList')}/>
+                        </View>
+
+                        <View style={styles.container}>
+                            <Button title= 'Practica ImageBackground & SlapshScreen' onPress={() => setScreen('Image')}/>
+                        </View>
+
+                        <View style={styles.container}>
+                            <Button title= 'Practica ActivityIndicator & keyboardAvoiding' onPress={() => setScreen('Activity')}/>
+                        </View>
+
+                        <View style={styles.container}>
+                            <Button title= 'Practica Modal & Bottom sheet' onPress={() => setScreen('Modal')}/>
+                        </View>
 
 
-                        
-                    <StatusBar style="auto" />
-                    </View>
+                        <StatusBar style="auto" />
+
+
+                    </>
                 );
                 }
 
---
+++ Practica5/miApp/screens/TextInputAlertScreen.js
@@ -0,0 +1,29 @@
+/*ZONA1: importaciones compponentes y archivos*/
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+
+export default function SafeAreaScreen() {
+return (
+    <View style={styles.container}>
+
+        <Text>Aqui va la practica de TextInput & Alert</Text>
+        
+    <StatusBar style="auto" />
+    </View>
+);
+}
+
+/*ZONA3: estilos y posicionamientos*/
+
+const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column-reverse',
+},
+
+});
