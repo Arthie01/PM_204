259e3823c63308bf109865b5d35cc2dc8a06fb5e
Arthie01 <123045452@upq.edu.mx>
2026-06-18 08:58:44 -0600

    ScreenView and ScroolView

--- Practica5/miApp/screens/MenuScreen.js
+++ Practica5/miApp/screens/MenuScreen.js
@@ -2,7 +2,7 @@
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import TarjetaScreen from './TarjetaScreen';
 import SafeAreaScreen from './SafeAreaScreen';
 import PressableScreen from './PressableScreen';
@@ -14,79 +14,75 @@ import ModalBottomScreen from './ModalBottomScreen';
 export default function MenuScreen() {
     const [screen, setScreen] = useState('menu');
 
-    switch(screen){
+    switch (screen) {
         case 'tarjetas':
-            return <TarjetaScreen/>
+            return <TarjetaScreen />;
         case 'SafeArea':
-            return <SafeAreaScreen/>
+            return <SafeAreaScreen />;
         case 'Pressable':
-            return <PressableScreen/>
+            return <PressableScreen />;
         case 'TextInput':
-            return <TextInputAlertScreen/>
+            return <TextInputAlertScreen />;
         case 'FlatList':
-            return <FlatListSectionScreen/>
+            return <FlatListSectionScreen />;
         case 'Image':
-            return <ImageBackgroundSlapshScreen/>
+            return <ImageBackgroundSlapshScreen />;
         case 'Activity':
-            return <ActivityIndicatorScreen/>
+            return <ActivityIndicatorScreen />;
         case 'Modal':
-            return <ModalBottomScreen/>
+            return <ModalBottomScreen />;
         case 'menu':
-            default:
+        default:
+            return (
+                <View style={styles.menuContainer}>
+                    <View style={styles.container}>
+                        <Button title='Practica Tarjetas' onPress={() => setScreen('tarjetas')} />
+                    </View>
 
-                return (
-                    <>
-                        <View style={styles.container}>
-                            <Button title= 'Practica Tarjetas' onPress={() => setScreen('tarjetas')}/>
-                        </View>
+                    <View style={styles.container}>
+                        <Button title='Practica SafeArea' onPress={() => setScreen('SafeArea')} />
+                    </View>
 
-                        <View style={styles.container}>
-                            <Button title= 'Practica SafeArea' onPress={() => setScreen('SafeArea')}/>
-                        </View>
+                    <View style={styles.container}>
+                        <Button title='Practica Pressable & switch' onPress={() => setScreen('Pressable')} />
+                    </View>
 
-                        <View style={styles.container}>
-                            <Button title= 'Practica Pressable & swtich' onPress={() => setScreen('Pressable')}/>
-                        </View>
+                    <View style={styles.container}>
+                        <Button title='Practica TextInput & Alert' onPress={() => setScreen('TextInput')} />
+                    </View>
 
-                        <View style={styles.container}>
-                            <Button title= 'Practica TextInput & Alert' onPress={() => setScreen('TextInput')}/>
-                        </View>
+                    <View style={styles.container}>
+                        <Button title='Practica FlatList & section' onPress={() => setScreen('FlatList')} />
+                    </View>
 
-                        <View style={styles.container}>
-                            <Button title= 'Practica FlatList & section' onPress={() => setScreen('FlatList')}/>
-                        </View>
+                    <View style={styles.container}>
+                        <Button title='Practica ImageBackground & SplashScreen' onPress={() => setScreen('Image')} />
+                    </View>
 
-                        <View style={styles.container}>
-                            <Button title= 'Practica ImageBackground & SlapshScreen' onPress={() => setScreen('Image')}/>
-                        </View>
+                    <View style={styles.container}>
+                        <Button title='Practica ActivityIndicator & keyboardAvoiding' onPress={() => setScreen('Activity')} />
+                    </View>
 
-                        <View style={styles.container}>
-                            <Button title= 'Practica ActivityIndicator & keyboardAvoiding' onPress={() => setScreen('Activity')}/>
-                        </View>
+                    <View style={styles.container}>
+                        <Button title='Practica Modal & Bottom sheet' onPress={() => setScreen('Modal')} />
+                    </View>
 
-                        <View style={styles.container}>
-                            <Button title= 'Practica Modal & Bottom sheet' onPress={() => setScreen('Modal')}/>
-                        </View>
-
-
-                        <StatusBar style="auto" />
-
-
-                    </>
-                );
-                }
-
+                    <StatusBar style="auto" />
+                </View>
+            );
+    }
 }
 
 /*ZONA3: estilos y posicionamientos*/
 
 const styles = StyleSheet.create({
-container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column-reverse',
-},
-
+    menuContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+    },
+    container: {
+        paddingVertical: 8,
+        alignItems: 'center',
+    },
 });
--- Practica5/miApp/screens/SafeAreaScreen.js
+++ Practica5/miApp/screens/SafeAreaScreen.js
@@ -1,29 +1,65 @@
 /*ZONA1: importaciones compponentes y archivos*/
 
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function SafeAreaScreen() {
-return (
-    <View style={styles.container}>
+    const [mostrarMensaje, setMostrarMensaje] = useState(true);
 
-        <Text>Aqui va la practica de SafeAreaView</Text>
-        
-    <StatusBar style="auto" />
-    </View>
-);
+    return (
+        <SafeAreaView style={style.safe} edges={['top', 'bottom']}>
+            <View style={style.encabezado}>
+                <Text style={style.texto}>ESTO ES UN ENCABEZADO</Text>
+            </View>
+
+            {mostrarMensaje && (
+                <View style={style.mensaje}>
+                    <Text style={style.MensajeTexto}>ESTO ES UN MENSAJE</Text>
+                    <TouchableOpacity onPress={() => setMostrarMensaje(false)}>
+                        <Text style={style.mensajeCerrar}>x</Text>
+                    </TouchableOpacity>
+                </View>
+            )}
+
+            <ScrollView style={style.scroll}
+            contentContainerStyle={style.listaContenido}
+            showsVerticalScrollIndicator={false}>
+                {['comprar pan', 'estudiar react native', 'aprender importaciones', 'llamar a alguien', 'revisar un correo', 'leer un libro', 'practicar guitarra',
+                'sacar a pasear el perro', 'hacer la tarea'
+                ].map((tarea, i) => (
+                    <View key = {i} style = {style.tarjeta}>
+                        <Text style={style.tarjetaTexto}>{tarea}</Text>
+                    </View>
+                ))}
+
+            </ScrollView>
+
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
 }
 
 /*ZONA3: estilos y posicionamientos*/
 
-const styles = StyleSheet.create({
-container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column-reverse',
-},
+const style = StyleSheet.create({
+    safe: { flex: 1, backgroundColor: 'blue' },
+    encabezado: { padding: 20, backgroundColor: 'red', alignItems: 'center', justifyContent: 'center' },
+    texto: { color: 'white', fontSize: 20, fontWeight: 'bold' },
 
-});
+    mensaje: {
+        flexDirection: 'row',
+        padding: 20,
+        backgroundColor: 'green',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    MensajeTexto: { color: 'white', fontSize: 20, fontWeight: 'bold' },
+    mensajeCerrar: { color: 'white', fontSize: 20, fontWeight: 'bold', marginLeft: 20 },
+
+    scroll: {flex:1},
+    listaContenido: {padding: 16, paddingBottom: 40},
+    tarjeta: {backgroundColor: '#f4f4f4f4', borderRadius: 10, padding:16, marginBottom: 10},
+    tarjetaTexto: {fontSize: 15, color: 'black'},
+})
